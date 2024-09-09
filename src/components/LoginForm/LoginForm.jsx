6b3bf2159dba1020d4cb944421efe309f3fb78c1
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-09-09 07:32:13 +0300

    login utilizabil

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,28 +1,56 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-function LoginForm() {
-  const navigate = useNavigate();
+function LoginForm({ onSubmit }) {
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Invalid email format')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+  });
 
-  const handleRegisterClick = e => {
-    e.preventDefault();
-    navigate('/register');
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: values => {
+      // Odată ce formularul este trimis cu succes, putem apela `onSubmit` pentru a închide modalul
+      onSubmit(values);
+    },
+  });
 
   return (
-    <form>
+    <form onSubmit={formik.handleSubmit}>
       <label>Email</label>
-      <input type="email" />
+      <input
+        type="email"
+        name="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div className="error">{formik.errors.email}</div>
+      ) : null}
 
       <label>Password</label>
-      <input type="password" />
+      <input
+        type="password"
+        name="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div className="error">{formik.errors.password}</div>
+      ) : null}
 
       <button type="submit">LOG IN</button>
-
-      {/* Buton pentru Register */}
-      <button type="button" onClick={handleRegisterClick}>
-        REGISTER
-      </button>
     </form>
   );
 }
